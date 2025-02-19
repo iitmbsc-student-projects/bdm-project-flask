f828e1b7fcb9bdee2d03ee3aeb08c7eb038e7c72
Ravindra Jain <ravindrajain045@gmail.com>
2025-02-19 12:21:35 +0530

    added host and port in app.run

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ if __name__ == "__main__":
     print(f"Total number of PDFs present: {total_pdfs}")
     print(f"Total number of split documents: {len(split_documents)}")
     print(f"Total number of split documents stored: {total_splits}")
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
